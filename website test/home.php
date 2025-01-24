0f7068e75ced2dba8b994dfcf8789898fe49dc04
youssefgharbi1 <150081674+youssefgharbi1@users.noreply.github.com>
2025-01-24 18:06:20 +0100

    added database file
    added deletion feature

--- website test/home.php
+++ website test/home.php
@@ -28,38 +28,6 @@
 
     include("form.html");
 
-    $querry = "SELECT * FROM vehicule WHERE email = '{$_SESSION['email']}'";
-    include("database.php");
-    $result = mysqli_query($db_conn,$querry);
-    if ($result && mysqli_num_rows($result) != 0){
-        echo "<div class='container' id = 'vehicule-container'>";
-        while($row = mysqli_fetch_assoc($result)){
-            
-            echo "<div class='vehicule'>";
-            echo "<img src='data:image/jpeg;base64,".base64_encode($row['imageData'])."'/>";
-            echo "<p>nom: {$row['name']}</p>";
-            echo "<p>immatriculation: {$row['immat']}</p>";
-            echo "<p>type: {$row['type']}</p>";
-            echo "<p>marque: {$row['marque']}</p>";
-            echo "<p>modele: {$row['modele']}</p>";
-            echo "<p>carburant: {$row['fuel']}</p>";
-            echo "<p>année de fabrication: {$row['manufactureYear']}</p>";
-            echo "</div>";
-            
-        }
-        echo "</div>";
-    }
-    ?>
-    
-    <form action="home.php" method="post">
-        <input type="submit" name="log_out" value="déconnexion">
-    </form>
-
-</body>
-</html>
-
-
-<?php
     if (isset($_POST["confirm"])){
         $name = htmlspecialchars($_POST["nom"]);
         $immat = htmlspecialchars($_POST["immat"]);
@@ -87,4 +55,108 @@
 
     }
 
-?>
+    ?>
+
+    <form action="home.php" method="post">
+        <input type="submit" name="show" value="afficher mes véhicules">
+    </form>
+    <?php
+    if (isset($_POST["show"])){
+        include("database.php");
+        $querry = "SELECT * FROM vehicule WHERE email = '{$_SESSION['email']}'";
+        $result = mysqli_query($db_conn,$querry);
+        if ($db_conn->query($querry) === false) {
+            echo "Error: " . $db_conn->error;
+        } 
+        if ($result && mysqli_num_rows($result) != 0){
+            echo "<div class='container' id = 'vehicule-container'>";
+            while($row = mysqli_fetch_assoc($result)){
+                
+                echo "<div class='vehicule'>";
+                echo "<img src='data:image/jpeg;base64,".base64_encode($row['imageData'])."'/>";
+                echo "<p>nom: {$row['name']}</p>";
+                echo "<p>immatriculation: {$row['immat']}</p>";
+                echo "<p>type: {$row['type']}</p>";
+                echo "<p>marque: {$row['marque']}</p>";
+                echo "<p>modele: {$row['modele']}</p>";
+                echo "<p>carburant: {$row['fuel']}</p>";
+                echo "<p>année de fabrication: {$row['manufactureYear']}</p>";
+                echo "</div>";
+                
+            }
+
+            echo "</div>";
+        }else{  
+            echo "<p>no vehicule found</p>";
+        }
+        mysqli_close($db_conn);
+    }
+    
+    
+    ?>
+
+    <h2>efacer vehicule</h2>
+    <form action="home.php" method="post">
+        <label for="">donner immatriculation:</label>
+        <input type="text" name="deletevehicule" value="">
+        <input type="submit" name="confirme" value="confirmer">
+    </form>
+    <?php
+
+    if (isset($_POST["confirme"])){
+        $deleteImmat = htmlspecialchars($_POST["deletevehicule"]);
+        include("database.php");
+        $querry = "DELETE FROM vehicule WHERE immat = '$deleteImmat'";
+        if ($db_conn->query($querry) === false) {
+            echo "Error: " . $db_conn->error;
+        } 
+        mysqli_close($db_conn);
+    }
+    ?>
+
+    <h2>chercher une vehicule</h2>
+    <form action="home.php" method="post">
+        <label for="">Search user by email:</label>
+        <input type="email" name="searchEmail" value="">
+        <input type="submit" name="search" value="search">
+    </form>
+
+    <?php
+    if (isset($_POST["search"])){
+        $searchEmail = htmlspecialchars($_POST["searchEmail"]);
+        $querry = "SELECT * FROM vehicule WHERE email = '$searchEmail'";
+        include("database.php");
+        $result = mysqli_query($db_conn,$querry);
+        if ($result && mysqli_num_rows($result) != 0){
+            echo "<div class='container' id = 'vehicule-container'>";
+            while($row = mysqli_fetch_assoc($result)){
+                
+                echo "<div class='vehicule'>";
+                echo "<img src='data:image/jpeg;base64,".base64_encode($row['imageData'])."'/>";
+                echo "<p>nom: {$row['name']}</p>";
+                echo "<p>immatriculation: {$row['immat']}</p>";
+                echo "<p>type: {$row['type']}</p>";
+                echo "<p>marque: {$row['marque']}</p>";
+                echo "<p>modele: {$row['modele']}</p>";
+                echo "<p>carburant: {$row['fuel']}</p>";
+                echo "<p>année de fabrication: {$row['manufactureYear']}</p>";
+                echo "</div>";
+                
+            }
+            echo "</div>";
+            mysqli_close($db_conn);
+
+        }else{  
+            echo "<p>no vehicule found</p>";
+        }
+    }
+    
+    ?>
+    <form action="home.php" method="post">
+        <input type="submit" name="log_out" value="déconnexion">
+    </form>
+
+</body>
+</html>
+
+
